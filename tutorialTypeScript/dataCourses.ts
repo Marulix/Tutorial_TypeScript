46945705d2ea0ff6bbd0591b747482360c977481
Mariana Ruiz <m.ruizg@uniandes.edu.co>
2022-10-22 20:35:48 -0500

    Todo funciona

---
+++ tutorialTypeScript/dataCourses.ts
@@ -0,0 +1,13 @@
+
+import { Course } from './course.js';
+
+export const dataCourses = [
+  new Course("Desarrollo de SW en Equipo", "José Bocanegra", 3),
+  new Course("Sistemas Transaccionales", "Oscar Ávila", 3),
+  new Course("Probabilidad y Estadistica", "Daniela Ortiz", 3),
+  new Course("Diseño y Análisis de Algoritmos", "Jorge Duitama", 3),
+  new Course("Infraestructura Computacional", "Sandra Rueda", 3),
+  new Course("Arquitectura Empresarial", "Paula Velandia", 3),
+  new Course("Francés 1", "María José Sierra", 3)
+] 
+  
